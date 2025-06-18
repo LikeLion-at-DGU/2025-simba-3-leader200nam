f8d5da365cb3d6b731dfe04034a2dc918fe5af19
minj0206 <alssl03@naver.com>
2025-06-18 22:56:15 +0900

    충돌 예시

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,7 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('home/', views.mainpage, name='mainpage'),
     path('intro/', views.intropage, name='intropage'),
 ]
