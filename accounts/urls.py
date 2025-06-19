05956ea431a8ab66792c3a92075e75d4b65293fd
minj0206 <alssl03@naver.com>
2025-06-19 18:15:54 +0900

    feat: swagger ui 적용 및 사용을 위한 drf 형식 변환

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    # DRF API
+    path('api/register/', views.RegisterAPIView.as_view(), name='api_register'),
+    path('api/login/', views.LoginAPIView.as_view(), name='api_login'),
+    path('api/logout/', views.LogoutAPIView.as_view(), name='api_logout'),
 ] 
