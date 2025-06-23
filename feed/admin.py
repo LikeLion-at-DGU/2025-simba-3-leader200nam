5ed75f875de83ede086ee27c85a91a16d5225e47
Soi-coding <2023112594@dgu.ac.kr>
2025-06-23 11:12:12 +0900

    피드페이지

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Feed, Comment, Like, Report
 
 # Register your models here.
+admin.site.register(Feed)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Report)
